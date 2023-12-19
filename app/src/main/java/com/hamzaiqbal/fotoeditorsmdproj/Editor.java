4a7987be8e699bbebe0f1764f08e5a53a46417cc
Hamza Iqbal <hamza.cstn@gmail.com>
2023-12-20 03:10:22 +0500

    added buttons to rotate image

--- app/src/main/java/com/hamzaiqbal/fotoeditorsmdproj/Editor.java
+++ app/src/main/java/com/hamzaiqbal/fotoeditorsmdproj/Editor.java
@@ -2,30 +2,65 @@ package com.hamzaiqbal.fotoeditorsmdproj;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 
 public class Editor extends AppCompatActivity {
 
     private ImageView imageView;
+    private Bitmap currentBitmap; // To hold the current bitmap
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor); // Make sure you have a layout named activity_editor
+        setContentView(R.layout.activity_editor);
 
-        imageView = findViewById(R.id.imageView); // Make sure you have an ImageView with this id in activity_editor.xml
+        imageView = findViewById(R.id.imageView);
+        Button rotateLeftButton = findViewById(R.id.button_rotate_left);
+        Button rotateRightButton = findViewById(R.id.button_rotate_right);
 
         Intent intent = getIntent();
-        if (intent.hasExtra("bitmap")) {
-            Bitmap bitmap = intent.getParcelableExtra("bitmap");
-            imageView.setImageBitmap(bitmap);
-        } else if (intent.hasExtra("uri")) {
+        if (intent.hasExtra("uri")) {
             Uri imageUri = Uri.parse(intent.getStringExtra("uri"));
             imageView.setImageURI(imageUri);
+            // Load the bitmap from the URI for manipulation
+            try {
+                currentBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        rotateLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotateImage(-90); // Rotate left
+            }
+        });
+
+        rotateRightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotateImage(90); // Rotate right
+            }
+        });
+    }
+
+    private void rotateImage(int degrees) {
+        if (currentBitmap != null) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(degrees);
+            currentBitmap = Bitmap.createBitmap(currentBitmap, 0, 0, currentBitmap.getWidth(), currentBitmap.getHeight(), matrix, true);
+            imageView.setImageBitmap(currentBitmap);
         }
     }
 }
