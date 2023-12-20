cb0b67e0f8049d4ca8639f65763ff656f5140441
Saba Karim <i200813@nu.edu.pk>
2023-12-20 22:14:01 +0500

    Added Filters and splash screen

--- app/src/main/java/com/hamzaiqbal/fotoeditorsmdproj/Editor.java
+++ app/src/main/java/com/hamzaiqbal/fotoeditorsmdproj/Editor.java
@@ -3,10 +3,13 @@ package com.hamzaiqbal.fotoeditorsmdproj;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +20,17 @@ import com.yalantis.ucrop.model.AspectRatio;
 import java.io.File;
 import java.io.IOException;
 
-public class Editor extends AppCompatActivity {
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter;
 
-    private ImageView imageView;
+public class Editor extends AppCompatActivity implements FiltersFragment.FiltersFragmentListener {
+    private FiltersFragment filtersFragment;
+    private ImageView imageView, buttonApplyFilter;
+    private HorizontalScrollView horizontalScrollView;
     private Bitmap currentBitmap; // To hold the current bitmap
     private static final int UCROP_REQUEST_CODE = 3;
+    private FrameLayout filtersContainer;
+    private GPUImageFilter selectedFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,10 @@ public class Editor extends AppCompatActivity {
         ImageView button_crop = findViewById(R.id.button_crop);
         ImageView button_rotate_left = findViewById(R.id.button_rotate_left);
         ImageView button_rotate_right = findViewById(R.id.button_rotate_right);
-
+        filtersContainer = findViewById(R.id.fragment_container);
+        ImageView button_filter = findViewById(R.id.button_filter);
+        horizontalScrollView = findViewById(R.id.edit_icons);
+        buttonApplyFilter = findViewById(R.id.button_apply_filter);
         Intent intent = getIntent();
         if (intent.hasExtra("uri")) {
             Uri imageUri = Uri.parse(intent.getStringExtra("uri"));
@@ -67,6 +79,32 @@ public class Editor extends AppCompatActivity {
                 }
             }
         });
+
+        // Set the click listener for the filter button
+        button_filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Hide the icons menu
+                horizontalScrollView.setVisibility(View.GONE);
+                // Show the filters fragment
+                showFiltersFragment(); // This should be called to show the FiltersFragment
+            }
+        });
+        buttonApplyFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The filter has already been applied; now we just hide the fragment and show the icons menu
+                if (filtersFragment != null) {
+                    getSupportFragmentManager().beginTransaction().hide(filtersFragment).commit();
+                }
+                filtersContainer.setVisibility(View.GONE); // Hide the container
+                horizontalScrollView.setVisibility(View.VISIBLE); // Show the icon menu
+                buttonApplyFilter.setVisibility(View.GONE); // Hide the "tick" button
+                // Now we save the state of the current bitmap with the applied filter
+                currentBitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+            }
+        });
+
     }
 
     private void rotateImage(int degrees) {
@@ -119,4 +157,80 @@ public class Editor extends AppCompatActivity {
             cropError.printStackTrace(); // Handle this properly in production code
         }
     }
+
+    private void showFiltersFragment() {
+        filtersFragment = (FiltersFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (filtersFragment == null) {
+            filtersFragment = new FiltersFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, filtersFragment)
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .show(filtersFragment)
+                    .commit();
+        }
+        filtersContainer.setVisibility(View.VISIBLE); // Ensure the container is visible
+    }
+    @Override
+    public void onFilterSelected(GPUImageFilter filter) {
+        selectedFilter = filter; // Store the selected filter
+        applyFilterToImage(); // Apply the filter immediately to the image
+        // Show the "tick" button to confirm the application of the filter
+        buttonApplyFilter.setVisibility(View.VISIBLE);
+    }
+
+    private void applyFilterToImage() {
+        if (selectedFilter != null && currentBitmap != null) {
+            GPUImage gpuImage = new GPUImage(this);
+            gpuImage.setImage(currentBitmap); // Set the current image
+            gpuImage.setFilter(selectedFilter); // Set the selected filter
+            Bitmap filteredBitmap = gpuImage.getBitmapWithFilterApplied(); // Get the filtered bitmap
+            imageView.setImageBitmap(filteredBitmap); // Update the ImageView with the filtered bitmap
+            // No need to set currentBitmap here if we're not saving the state yet
+        }
+    }
+
 }
+
+    // Initialize this in your onCreate or wherever appropriate
+//    private void setupApplyFilterButton() {
+//        buttonApplyFilter = findViewById(R.id.button_apply_filter);
+//        buttonApplyFilter.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Hide the FiltersFragment and show the icon menu
+//                if (filtersFragment != null) {
+//                    getSupportFragmentManager().beginTransaction().hide(filtersFragment).commit();
+//                }
+//                filtersContainer.setVisibility(View.GONE); // Hide the container
+//                horizontalScrollView.setVisibility(View.VISIBLE); // Show the icon menu
+//                // The filter has already been applied, just hide the "tick" button
+//                buttonApplyFilter.setVisibility(View.GONE);
+//            }
+//        });
+//    }
+//}
+//    @Override
+//    public void onFilterSelected(GPUImageFilter filter) {
+//        selectedFilter = filter; // Store the selected filter
+//        applyFilterToImage(); // Apply the filter to the image
+//        // Hide the FiltersFragment
+//        if (filtersFragment != null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .hide(filtersFragment)
+//                    .commit();
+//            filtersContainer.setVisibility(View.GONE); // Hide the container
+//        }
+//    }
+
+//    private void applyFilterToImage() {
+//        if (currentBitmap != null && selectedFilter != null) {
+//            GPUImage gpuImage = new GPUImage(this);
+//            gpuImage.setImage(currentBitmap);
+//            gpuImage.setFilter(selectedFilter);
+//            Bitmap filteredBitmap = gpuImage.getBitmapWithFilterApplied();
+//            imageView.setImageBitmap(filteredBitmap);
+//        }
+//    }
+//}
---
+++ app/src/main/java/com/hamzaiqbal/fotoeditorsmdproj/Splash.java
@@ -0,0 +1,23 @@
+package com.hamzaiqbal.fotoeditorsmdproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    private static final long SPLASH_DELAY = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_DELAY);
+    }
+}
